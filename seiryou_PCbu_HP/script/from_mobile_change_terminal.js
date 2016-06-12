bd13f6ee92054c988f614e94deb4502e928cad28
ottiy <au1234555666777@gmail.com>
2016-06-12 18:44:35 +0900

    一気にいろいろ作った

---
+++ seiryou_PCbu_HP/script/from_mobile_change_terminal.js
@@ -0,0 +1,3 @@
+if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    
+}
